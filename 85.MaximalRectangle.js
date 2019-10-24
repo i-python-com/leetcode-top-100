4e86b3acf21417bd5ed8de38fc82ab253d846f77
i-python-com <wyp4wj@gmail.com>
2019-10-24 14:27:07 -0700

    Add 85.MaximalRectangle.js, matrix, array, stack, histogram area

---
+++ 85.MaximalRectangle.js
@@ -0,0 +1,58 @@
+/*-------------------------------matrix, array, stack, histogram area
+ * @param {character[][]} matrix
+ * @return {number}
+ */
+/*
+1.transfer first row of matrix to histogram, get max area
+2.update histogram with next row of matrix, update max area
+*/
+var maximalRectangle = function(matrix) {
+  if (!matrix.length || !matrix[0].length) {
+    return 0
+  }
+  const m = matrix.length
+  const n = matrix[0].length
+  const arr = new Array(n).fill(0)
+  let max = 0
+  for (let i = 0; i < m; i++) {
+    for (let j = 0; j < n; j++) {
+      arr[j] = matrix[i][j] === '1' ? arr[j] + 1 : 0
+    }
+
+    max = Math.max(max, findMax(arr))
+  }
+  return max
+}
+
+function findMax(heights) {
+  const stack = []
+  let max = 0
+  for (let i = 0; i < heights.length; i++) {
+    while (stack.length && heights[i] < heights[stack[stack.length - 1]]) {
+      const area = getArea(heights, stack, i)
+      max = Math.max(max, area)
+    }
+    stack.push(i)
+  }
+  while (stack.length) {
+    const area = getArea(heights, stack, heights.length)
+    max = Math.max(max, area)
+  }
+  return max
+}
+
+function getArea(heights, stack, i) {
+  const height = heights[stack.pop()]
+  const width = stack.length ? i - stack[stack.length - 1] - 1 : i
+  const area = width * height
+  return area
+}
+
+let matrix = [
+  ['1', '0', '1', '0', '0'], //
+  ['1', '0', '1', '1', '1'],
+  ['1', '1', '1', '1', '1'],
+  ['1', '0', '0', '1', '0']
+]
+
+console.log(maximalRectangle(matrix))
